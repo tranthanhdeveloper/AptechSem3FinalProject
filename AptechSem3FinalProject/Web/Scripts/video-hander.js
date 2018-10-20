64ef99bbe2a17093db8545934c6e8b5f290157ab
TRAN_THANH <tranthanhdeveloper@gmail.com>
2018-10-20 12:29:33 +0700

    Add static files, Js, css for template.
    + add Plyr.js library for load HTML5 video
    + add css, orther file

---
+++ AptechSem3FinalProject/Web/Scripts/video-hander.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => { 
+    // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+    const player = new Plyr('#player');
+  
+    // Bind event listener
+    function on(selector, type, callback) {
+      document.querySelector(selector).addEventListener(type, callback, false);
+    }
+  });
