c59fd58bce8a18a3a86b42ce801ba482872aaae8
PRIVATE\HoangDuc <hoangduc02011998>
2018-10-21 13:12:49 +0700

    get data

--- AptechSem3FinalProject/Web/Controllers/HomeController.cs
+++ AptechSem3FinalProject/Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Context.Database;
 
 namespace Web.Controllers
 {
@@ -10,6 +11,8 @@ namespace Web.Controllers
     {
         public ActionResult Index()
         {
+            var a = new AptechSem3FinalProjectEntities();
+            var c = a.Users.ToList();
             return View();
         }
     }
