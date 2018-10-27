82d61a7d302fe34ae61fc8c617c283fac4e943b1
TRAN_THANH <tranthanhdeveloper@gmail.com>
2018-10-27 22:27:40 +0700

    Add search to Header and Fixed Course detail style

---
+++ AptechSem3FinalProject/Web/Models/CourseViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Context.Database;
+
+namespace Web.Models
+{
+    public class CourseListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public long Price { get; set; }
+    }
+
+    public class CourseDetailViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Comment { get; set; }
+        public string Image { get; set; }
+        public long Price { get; set; }
+        public int Status { get; set; }
+        public virtual User User { get; set; }
+        public virtual List<Lecture> Lectures { get; set; }
+    }
+
+    public class CoursePlayViewModel
+    {
+
+    }
+}
